c034995b69a611c8ccaeb072e290b1fdb42b4993
muhammad_alif_d <muhammad_alif_d@students.itb.ac.id>
2022-08-06 10:29:24 +0700

    implement connect to metamask

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import '../assets/styles/Login.css'
 import bg from '../assets/image/login-bg.jpg'
 import { useNavigate } from 'react-router-dom';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
 
 export function LoginPage() {
   const navigate = useNavigate()
@@ -13,17 +15,20 @@ export function LoginPage() {
     window.ethereum.on('accountsChanged', function (accounts) {
       setWallet(accounts[0])
     })
-  }, [])
+    console.log(window.ethereum)
+  })
 
   const handleLogin = (e) => {
-    alert(nim, nama, wallet)
     navigate('/dashboard')
   }
 
   const connectWallet = async (e) => {
-    const { ethereum } = window
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-    setWallet(accounts[0])
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setWallet(address)
   }
 
   return (
@@ -32,12 +37,12 @@ export function LoginPage() {
         <h1>Sign In</h1>
         <form onSubmit={handleLogin} className='login-form'>
           <label for='nim'>NIM</label>
-          <input type={'text'} name='nim' id='nim' placeholder='NIM Anda' onChange={(e) => setNim(e.target.value)}/>
+          <input required type={'text'} name='nim' id='nim' placeholder='NIM Anda' onChange={(e) => setNim(e.target.value)}/>
           <label for='nama'>Nama</label>
-          <input type={'text'} name='nama' id='nama' placeholder='Nama Anda' onChange={(e) => setNama(e.target.value)}/>
+          <input required type={'text'} name='nama' id='nama' placeholder='Nama Anda' onChange={(e) => setNama(e.target.value)}/>
           <label for='nama'>Wallet Address</label>
           <div className='input-group'>
-            <input type={'text'} name='wallet' value={wallet} id='wallet' disabled placeholder='0x123..' />
+            <input required type={'text'} name='wallet' value={wallet} id='wallet' disabled placeholder='0x123..' />
             <button type='button' onClick={connectWallet}>Connect Wallet</button>
           </div>
 
--- src/components/widgets/Header.js
+++ src/components/widgets/Header.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
 
 export function Header() {
+  const [wallet, setWallet] = useState('')
+  const [balance, setBalance] = useState('')
+  const json = require('../../assets/chains.json')
+
+  useEffect(() => {
+    window.ethereum.on('accountsChanged', function (accounts) {
+      setWallet(accounts[0])
+      getBalance()
+    });
+    window.ethereum.on("chainChanged", function(chain) {
+      getBalance()
+    });
+    connect();
+    getBalance();
+  })
+
+  const connect = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setWallet(address)
+  }
+
+  const getBalance = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const balance = await signer.getBalance()
+    const result = (balance / 1e18).toFixed(4)
+    const currency = await provider.getNetwork()
+    setBalance(`${result.toString()} ${getChainDetail(currency.chainId).nativeCurrency.symbol}`)
+  }
+
+  const getChainDetail = (chainId) => {
+    const detail = json.find(item => item.chainId === chainId)
+    return detail
+  }
+  
   return (
     <header>
       <h1>Sertifikat</h1>
-      <div className='account'></div>
+      <div className='account'>
+        <h3>Lorem Ipsum</h3>
+        <p>{wallet}</p>
+        <p>{balance}</p>
+      </div>
     </header>
   )
 }
